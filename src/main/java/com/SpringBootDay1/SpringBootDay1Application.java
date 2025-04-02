a73a0f7ec6511c825859d1711eb7183badb89fa7
DESKTOP-3DOP2EC\ADMIN <rinkusalunke9@gmail.com>
2025-04-02 23:06:05 +0530

    1st commit

---
+++ src/main/java/com/SpringBootDay1/SpringBootDay1Application.java
@@ -0,0 +1,13 @@
+package com.SpringBootDay1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDay1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDay1Application.class, args);
+		System.out.println("springboot");
+	}
+}
